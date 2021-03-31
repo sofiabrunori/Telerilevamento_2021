16622a4dfeea015d2e4f0b92c2dae06e5dda87fe
sofiabrunori <62289023+sofiabrunori@users.noreply.github.com>
2021-03-31 13:01:23 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,4 @@
+# TIME SERIES ANALISYS
+# SB: scaricare i dati da virtuale e metterli in una cartella dentro lab
+library(raster) #SB va richiamata sempre prima la libreria
+setwd("C:/lab/greenland") # SB: settare la wd mettendo bene la cartella
