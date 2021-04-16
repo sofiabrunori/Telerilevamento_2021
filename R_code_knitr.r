78e3dda96b84474f12132c65cef2f476a1c290c5
sofiabrunori <62289023+sofiabrunori@users.noreply.github.com>
2021-04-16 10:56:30 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -2,4 +2,10 @@
 setwd("C:/lab/") #SB: come sempre prima si setta la W.D
 require(knitr) #SB: il comando require ha la stesso funzione di library
 #SB: knitr prende un codice esterno che va messo tra virgolette e all'interno di R genera un report che si salva nella cartella dove è presente il codice
-
+#SB: copia lo script in una pagina word e salvala nella cartella lab
+#SB: la funzione che lo permette è stitch e il primo argomento è il nome del codice che ho salvato in lab
+install.packages("tinytex")
+stitch("R_code_greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+#SB: da errore ma si risolve così
+tinytex::install_tinytex()
+tinytex::tlmgr_update()
