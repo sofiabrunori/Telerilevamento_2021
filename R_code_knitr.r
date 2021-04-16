450c13c1c6dbb569b38cbf05736808521d70d6ce
sofiabrunori <62289023+sofiabrunori@users.noreply.github.com>
2021-04-16 09:32:50 +0200

    Create R_code_knitr.r

---
+++ R_code_knitr.r
@@ -0,0 +1,5 @@
+# 16/04/21 codice per creare file 
+setwd("C:/lab/") #SB: come sempre prima si setta la W.D
+require(knitr) #SB: il comando require ha la stesso funzione di library
+#SB: knitr prende un codice esterno che va messo tra virgolette e all'interno di R genera un report che si salva nella cartella dove è presente il codice
+
