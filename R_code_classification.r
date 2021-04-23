d28a5a4038c493551e315805fee3a09bc12ac191
sofiabrunori <62289023+sofiabrunori@users.noreply.github.com>
2021-04-23 10:09:47 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -1,6 +1,8 @@
 #SB: classificazione di alcune immagini del sole 21/04
 library(raster) #SB: richiamiamo la libreria raster che mi permette di usare file raster
 library(RStoolbox) #SB: anche RStoolbox
+setwd("C:/lab/") #SB: settiamo la wd (ricordati che se usi il mec cambia la stringalibrary(raster) #SB: richiamiamo la libreria raster che mi permette di usare file raster
+library(RStoolbox) #SB: anche RStoolbox
 setwd("C:/lab/") #SB: settiamo la wd (ricordati che se usi il mec cambia la stringa
 so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg") #SB: carichiamo l'immagine all'interno di r con la funzione brick, e con la freccetta gli associo il nome
 so #SB: se digiiamo il nome r mi restituisce le informazioni del dato
@@ -44,4 +46,37 @@ gatto <- brick("prova.jpg")
 gattoC <- unsuperClass(gatto, nClasses= 3)
 cl <- colorRampPalette(c('red','black','yellow'))(100)
 plot(gattoC$map, col=cl)
-## SB: facciamo un esercitazione con il grand canyon
+
+## SB: facciamo un esercitazione con il grand canyon per visualizzare alcune litologie
+#SB: i dati sono stati scaricati da: https://virtuale.unibo.it/mod/resource/view.php?id=558031
+library(raster) #SB: richiamiamo la libreria raster che mi permette di usare file raster
+library(RStoolbox) #SB: i pacchetti si possono richiamare anche con require()
+setwd("C:/lab/") #SB: poi inseriamo la working directory
+#SB: importa l'immagine in RGB associando un nome con brick perchè l'immagine è rgb e brick le monta gia insieme
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+plotRGB(gc, r=1, g=2, b=3, stretch="lin") #SB: plotRGB permette di plottare un file in tre livelli per vederlo come l'occhio umano (B=3, G=2, R=1)
+plotRGB(gc, r=1, g=2, b=3, stretch="hist") #SB: cambio lo stretching per cambiare la visualizzazione
+#SB: i daltonici non distinguono il rosso ed il verde quindi in un articolo meglio non usarli
+#SB: facciamo una classificazione non supervisionata in 2 classi, sarà diversa per ogni pc 
+#SB: info a: https://cran.r-project.org/web/packages/RStoolbox/RStoolbox.pdf
+gcc2 <-unsuperClass(gc, nClasses= 2) #SB: due classi 
+gcc2
+#unsuperClass results
+
+#*************** Map ******************
+#$map
+#class      : RasterLayer 
+#dimensions : 6222, 9334, 58076148  (nrow, ncol, ncell)
+#resolution : 1, 1  (x, y)
+#extent     : 0, 9334, 0, 6222  (xmin, xmax, ymin, ymax)
+#crs        : NA 
+#source     : C:/Users/utente/AppData/Local/Temp/RtmpmAz4CL/raster/r_tmp_2021-04-23_095124_10140_90209.grd 
+#names      : layer 
+#values     : 1, 2  (min, max)
+plot(gcc2$map)#SB: per plottare ricordati di legare il dataset alla mappa con $map
+gcc4 <- unsuperClass(gc, nClasses=4) #SB: riclassifichiamo in 4 classi
+plot(gcc4$map)
+
+
+
+
