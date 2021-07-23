311f61450dcd09ecc6d4a30586956b34e35738f2
sofiabrunori <62289023+sofiabrunori@users.noreply.github.com>
2021-07-23 16:13:23 +0200

    Update progetto_esame.r

--- progetto_esame.r
+++ progetto_esame.r
@@ -48,21 +48,27 @@ par(mfrow=c(2,1))
 plot(preclass$map, col=clclassp, main= "situazione pre alluvione")
 plot(postclass$map, col=clclassd, main= "situazione post alluvione")
 ############################################################################################
+
+
 freq(preclass$map)
 #    value  count
 #[1,]     1 147142
 #[2,]     2 259492
 #[3,]     3  25366
 freq(postclass$map)
-    value  count
+#    value  count
 #[1,]     1 108539
 #[2,]     2  43419
 #[3,]     3 280042
+
+
 #############################################################################
 p1 <- ggRGB(pre, r=1, g=2, b=3, stretch="lin")
 p2 <- ggRGB(post, r=1, g=2, b=3, stretch="lin")
 grid.arrange(p1, p2, nrow=2) 
 #######################################################################
+
+
 spre <- 147142 + 259492 + 25366
 spost <- 108539 + 43419 + 280042
 proppre <- freq(preclass$map) / spre
@@ -72,30 +78,41 @@ proppre <- freq(preclass$map) / spre
 #[3,] 6.944444e-06 0.05871759
 
 proppost <- freq(postclass$map) / spost
-            value     count
+ #          value     count
 #[1,] 2.314815e-06 0.2512477
 #[2,] 4.629630e-06 0.1005069
 #[3,] 6.944444e-06 0.6482454
 
 cover <- c("suolo inondato","suolo")
-percent_pre <- c(5.9, 94.1) #SB: la seconda colonna sono le percentuali 92, vanno messi i dati che vengono dal tuo pc
+percent_pre <- c(5.9, 94.1) 
 percent_post <- c(10, 90)
 percentages <- data.frame(cover, percent_pre, percent_post)
 percentages
- #         cover percent_pre percent_post
+#         cover percent_pre percent_post
 #1 suolo inondato         5.9           10
 #2          suolo        94.1           90
-ggplot(percentages, aes(x=cover, y=percent_pre, color=cover)) + geom_bar(stat="identity", fill="lightblue")
-ggplot(percentages, aes(x=cover, y=percent_post, color=cover)) + geom_bar(stat="identity", fill="lightblue")
-p1 <- ggplot(percentages, aes(x=cover, y=percent_pre, color=cover)) + geom_bar(stat="identity", fill="white")
-p2 <- ggplot(percentages, aes(x=cover, y=percent_post, color=cover)) + geom_bar(stat="identity", fill="white")
-grid.arrange(p1, p2, nrow=1)
+
+ggplot(percentages, aes(x=cover, y=percent_pre, color=cover)) + geom_bar(stat="identity", fill="springgreen3")
+ggplot(percentages, aes(x=cover, y=percent_post, color=cover)) + geom_bar(stat="identity", fill="darkolivgreen1")
+
+percent1 <- ggplot(percentages, aes(x=cover, y=percent_pre, color=cover)) + geom_bar(stat="identity", fill="springgreen3")
+percent2 <- ggplot(percentages, aes(x=cover, y=percent_post, color=cover)) + geom_bar(stat="identity", fill="darkolivegreen1")
+grid.arrange(percent1, percent2, nrow=1)
+
 ######################################################################################################################################################
+
 perc_inond <- c(5.9,10)
 cover <- c("suolo inondato_pre","suolo inondato_post")
 tab <- data.frame(perc_inond)
-p1 <- ggplot(tab, aes(x=cover, y=perc_inond, color=cover)) + geom_bar(stat="identity", fill="light blue")
+p1 <- ggplot(tab, aes(x=cover, y=perc_inond, color=cover)) + geom_bar(stat="identity", fill="seagreen4")
 grid.arrange(p1, nrow=1)
+
+
+clpie<- colorRampPalette(c("thistle","lightgreen"))(2)
+par(mfrow=c(1,2))
+piepre <-pie(percent_pre, main="Diagramma a torta",labels=" situazione pre-alluvione" ,col=clpie)
+piepost <-pie(percent_post, main="Diagramma a torta",labels=" situazione post-alluvione" ,col=clpie)
+
 
 ####################################################################################################################################################
 
@@ -111,6 +128,7 @@ import <- lapply(rlist,raster)
 Bande <- stack(import)
 plot(Bande, main=" bande della zona analizzata in precedenza")
 plotRGB(Bande, r=4, g=3, b=2, stretch="Lin")
+
 par(mfrow=c(2,2))
 clB<- colorRampPalette(c("black","blue","light blue","light grey")) (200)
 clV<- colorRampPalette(c("black","green","light green","light grey")) (200)
@@ -123,6 +141,8 @@ plot(Bande$SR_B5, col=clNIR, main="Banda del vicino-infrarosso")
 
 ########################################################################################################################################################
    #crop per vedere un focus in rgb della zona affetta da inondazione
+
+
 extension <- c(676000,725000 , 5660000, 5700000)
 bandecut<- crop(Bande, extension)
 plotRGB(bandecut, r=4, g=3, b=2, stretch="Lin")
@@ -131,6 +151,8 @@ plotRGB(bandecut, r=4, g=3, b=2, stretch="Lin")
 
 
 #calcolo dvi
+
+
 dvi <- Bande$SR_B5- Bande$SR_B4
 cldvi <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
 plot(dvi, col=cldvi, main="Calcolo DVI tramite la sottrazione della banda 4 alla 5")
@@ -138,19 +160,24 @@ plot(dvi, col=cldvi, main="Calcolo DVI tramite la sottrazione della banda 4 alla
 plot(dvi, col=cl)
 #calcolo NDVI
 #SB:NDVI=(NIR-RED) / (NIR+RED)
+
 ndvi <- dvi / (Bande$SR_B5+ Bande$SR_B4)
 clndvi <- colorRampPalette(c('grey','lightgreen','darkgreen'))(50)
 plot(ndvi, col=clndvi,main="Calcolo NDVI (DVI normalizzato)")
 
 #######################################################################################################################################################
 
-pairs(Bande) #SB: mi mostra la correlazione tra tutte le bande e anche il valore di Pearson
-banderes <- aggregate(Bande, fact=10) #SB: meglio ricampionare l'immagine con aggregate (in pratica abbasso la risoluzione) per evitare che le funzioni dopo impieghino troppo tempo
-banderes #SB: con fattore 10 la sisuluzione è passata da 30 a 300
- plotRGB(banderes, r=4, g=3, b=2, stretch="Lin") 
-vi<- spectralIndices(banderes, green = 3, red = 4, nir = 5) #???????
+ #SB: mi mostra la correlazione tra tutte le bande e anche il valore di Pearson
+banderes <- aggregate(Bande, fact=10) 
+banderes 
+plotRGB(banderes, r=4, g=3, b=2, stretch="Lin") 
+pairs(banderes)
+
+vi<- spectralIndices(banderes, green = 3, red = 4, nir = 5) 
 clvi <- colorRampPalette(c('cyan', 'purple', 'red')) (300)
-plot(vi, col=clvi, title="                 calcolo dei diversi vegetation index")
+plot(vi, col=clvi, title="calcolo dei diversi vegetation index")
+
+
 #############################################################################################################
 
     
